c8a1d61ba52adbb34842dd7be57828de7c33f4d2
Bastien CARCAT <bastien-carcat@orange.fr>
2025-10-21 10:59:41 +0200

    feat: add statistics placeholder

--- src/app/(root)/programs/[id]/page.tsx
+++ src/app/(root)/programs/[id]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ProgramInsightsCard } from "../_components/program-insights-card";
 
 export default async function ProgramPage({
   params,
@@ -9,13 +10,13 @@ export default async function ProgramPage({
 
   return (
     <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-semibold mb-4">Program Builder</h1>
-      <p className="text-muted-foreground mb-2">
-        Program ID: {id}
-      </p>
+      <h1 className="mb-4 text-3xl font-semibold">Program Builder</h1>
+      <p className="text-muted-foreground mb-2">Program ID: {id}</p>
       <p className="text-muted-foreground">
-        This is where you will be able to build and schedule your training program.
+        This is where you will be able to build and schedule your training
+        program.
       </p>
+      <ProgramInsightsCard />
     </div>
   );
 }
